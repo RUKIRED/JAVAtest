1fee4821af9e93142852e038d187ed140fca8079
user <user@localhost>
2026-02-05 18:07:42 +0900

    クラスファイル作成

---
+++ HelloWorld_HarukiKasa/src/sample/test.java
@@ -0,0 +1,15 @@
+package sample;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int result = add(5, 3); // メソッドを呼び出す
+		System.out.println(result); // 8
+	}
+
+	public static int add(int a, int b) {
+		return a + b;
+	}
+
+}
