68204f0e24241334e048dee13cfb8bd84ce233f6
user <user@localhost>
2026-02-06 14:05:06 +0900

    added comments

--- HelloWorld_HarukiKasa/src/curriculum/b/Animal.java
+++ HelloWorld_HarukiKasa/src/curriculum/b/Animal.java
@@ -1,30 +1,38 @@
 package curriculum.b;
 
 public class Animal {
+	//それぞれのデータの型を決める
 	String name;
 	double length;
 	int velocity;
 
+	//Stringのgetter
 	public String getName() {
 		return name;
 	}
 
+	//フィールドを指定
 	public void setName(String name) {
+		//nameの指定
 		this.name = name;
 	}
 
+	//lengthのgetter
 	public double getLength() {
 		return length;
 	}
 
+	//lingth.setter
 	public void setLength(double length) {
 		this.length = length;
 	}
 
+	//velocity.getter
 	public int getVelocity() {
 		return velocity;
 	}
 
+	//velocity.setter
 	public void setVelocity(int velocity) {
 		this.velocity = velocity;
 	}
