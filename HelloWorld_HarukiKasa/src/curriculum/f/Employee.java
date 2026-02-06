1e1611308341155ab366259ea3c33f29623e05b1
user <user@localhost>
2026-02-06 15:28:21 +0900

    name

---
+++ HelloWorld_HarukiKasa/src/curriculum/f/Employee.java
@@ -0,0 +1,11 @@
+package curriculum.f;
+
+public class Employee {
+	String employeeid;
+	String name;
+
+	public void showinfo() {
+		System.out.println("社員ID:" + employeeid + ",名前:" + name);
+	}
+
+}
